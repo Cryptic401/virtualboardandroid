18ff66b8fa14421495c7ff1d6d5f1f7aaa5f46e2
Wille Strengell <willestrengell@gmail.com>
2023-10-10 17:39:25 +0300

    update

---
+++ public/login/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const authorizeToken = require('auth')
+const PORT = process.env.PORT || 3030
+const app = express();
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.use('/', express.static(__dirname + '/public/login'))
+
+
+const usersEndpoint = require('./routes/users.js')
+app.use('/users', usersEndpoint)
+
+
+const boardsEndpoint = require('./routes/board.js')
+app.use('/board',authorizeToken, boardsEndpoint)
+
+const notesEndpoint = require('./routes/notes.js')
+app.use('/notes',authorizeToken, notesEndpoint)
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
+
+
